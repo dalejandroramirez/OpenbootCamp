daeb956e727893999692c2f9b41d335cdc7f53e4
Alejadro Ramirez <dalejandro.ramirez@udea.edu.co>
2022-10-03 16:58:21 -0500

    feature/ geetingFun
    Se agrego una funcion de saludo

--- src/index.js
+++ src/index.js
@@ -10,8 +10,6 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-import ReactDOM from 'react-dom/client';
-
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
